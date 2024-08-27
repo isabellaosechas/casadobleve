a08763560513f47757654c6ac5af476af73e9396
isabellaosechas <isabellaosechas@gmail.com>
2024-08-27 08:43:48 -0400

    cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./views/**/*.{html,js}"],
   theme: {
     extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        }
+      },
   },
   },
 };
