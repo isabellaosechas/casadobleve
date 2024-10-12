52ec17a471cd56891ea050d31982685403513609
isabellaosechas <isabellaosechas@gmail.com>
2024-10-12 00:07:25 -0400

    updates

---
+++ views/components/whatsapp.js
@@ -0,0 +1,9 @@
+const wppSection = document.querySelector('#wpp');
+
+const createWppSection = () => {
+    wppSection.innerHTML=`
+   <a href="https://wa.me/04128845888" target="_blank"><img src="/images/WhatsApp_icon.png" class="w-14 h-14"></a>
+    `;
+}
+
+createWppSection();
